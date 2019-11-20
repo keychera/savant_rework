9661850f0c630c6c8e554e659786ebc429a11164
Kevin Erdiza Yogatama <kyogatama@gmail.com>
2019-11-20 02:52:02 -0500

    add script to get all covered classes

--- artifacts/resavant-testrunner/run.sh
+++ artifacts/resavant-testrunner/run.sh
@@ -6,16 +6,19 @@ source runconfig
 #the steps
 
 #step 1:
-defects4j test -w $TARGETPROJECT
+#defects4j test -w $TARGETPROJECT
 
 #step 2: read the 'failing_tests'
-cp $TARGETPROJECT/failing_tests ./failing_tests_report
-python scripts/get_failing_tests.py ./failing_tests_report .
+#cp $TARGETPROJECT/failing_tests ./failing_tests_report
+#python scripts/get_failing_tests.py ./failing_tests_report .
 
 #step 3
-defects4j export -w "$TARGETPROJECT" -p dir.src.classes
-python scripts/get_java_classes_from_directory.py $TARGETPROJECT/src/main/java .
+#defects4j export -w "$TARGETPROJECT" -p dir.src.classes
+#python scripts/get_java_classes_from_directory.py $TARGETPROJECT/src/main/java .
 
-#step4+5 instrument all classes and run coverage of the failed test
-defects4j coverage -w $TARGETPROJECT -t $TESTCLASS::$TESTMETHOD -i all_classes
-python scripts/get_methods_coverage.py $TARGETPROJECT/coverage.xml .
+#step4+5+6 instrument all classes and run coverage of the failed test
+#defects4j coverage -w $TARGETPROJECT -t $TESTCLASS::$TESTMETHOD -i all_classes
+#cripts/get_methods_coverage.py $TARGETPROJECT/coverage.xml .
+
+#step7 get all classes
+python scripts/get_classes_coverage.py ./covered_methods
---
+++ artifacts/resavant-testrunner/scripts/get_classes_coverage.py
@@ -0,0 +1,26 @@
+import sys
+from bs4 import BeautifulSoup
+
+if len(sys.argv) >= 2:
+    report_path = sys.argv[1]
+
+    covered_methods = list()
+    with open(report_path) as fp:
+        for count, line in enumerate(fp):
+            covered_methods.append(line.rstrip())
+
+    covered_classes = list()
+    for method in covered_methods:
+        a_class = method.split('::')[0]
+        if not(a_class in covered_classes): 
+            covered_classes.append(a_class)
+        
+    
+    if len(sys.argv) == 3:
+        output_path = sys.argv[2]
+        with open(output_path + '/covered_classes', 'w+') as f:
+            for failing_test in covered_classes:
+                f.write("{}\n".format(failing_test))
+    else:
+        for failing_test in covered_classes:
+            print(failing_test)
