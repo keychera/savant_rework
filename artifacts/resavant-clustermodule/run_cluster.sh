ec47eb3e05debca38ca48c5c9d56010ef4220893
Kevin Erdiza Yogatama <kyogatama@gmail.com>
2019-12-02 00:58:16 -0500

    make scr for test selection

--- artifacts/resavant-clustermodule/run_cluster.sh
+++ artifacts/resavant-clustermodule/run_cluster.sh
@@ -2,3 +2,5 @@
 source runconfig
 
 python $PYSCRIPT_PATH/get_method_cluster.py "$TEMP/res.csv" 5 "$TEMP/clusters" 
+
+python $PYSCRIPT_PATH/select_tests.py "$TEMP/res.csv" "$TEMP/clusters" 10 "$TEMP/selected_tests"
