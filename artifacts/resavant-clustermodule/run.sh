fbfe6c9da6ec970aa2c6a943b3298d0c987a68e5
square <square@localhost.localdomain>
2019-12-04 23:23:52 +0900

    make script check py and dirs

--- artifacts/resavant-clustermodule/run.sh
+++ artifacts/resavant-clustermodule/run.sh
@@ -3,20 +3,38 @@ source runconfig
 
 mkdir -p $TEMP/
 
+# check target dir \$TARGETPROJECT
+if ! [ -d "$TARGETPROJECT" ]
+then
+    echo "the path: $TARGETPROJECT does not exist"
+    exit 1
+fi
+
+# check python command
+if [ -x "$(command -v python)" ]
+then
+    PY_COMMAND=python
+elif [ -x "$(command -v python3)" ]
+then
+    PY_COMMAND=python3
+else
+    echo 'Error: python is either not installed' >&2
+    echo 'or the command to call python is not' >&2
+    echo '`python` or `python3`' >&2
+    exit 1
+fi
+
 # step 1: get all tests, get all class
 git --git-dir=$TARGETPROJECT/.git/ --work-tree=$TARGETPROJECT clean -f -d
 defects4j test -w $TARGETPROJECT
-python $PYSCRIPT_PATH/get_all_test_methods.py $TARGETPROJECT/all_tests "$TEMP/all_tests"
-
-    # if using export (but it only output the test classes)
-    # defects4j export -w $TARGETPROJECT -p tests.all -o $TEMP/all_tests
-
+$PY_COMMAND $PYSCRIPT_PATH/get_all_test_methods.py $TARGETPROJECT/all_tests "$TEMP/all_tests"
+exit 1
 # step 2: read the 'failing_tests'
 cp $TARGETPROJECT/failing_tests $TEMP/failing_tests_report
-python $PYSCRIPT_PATH/get_failing_test_methods.py $TEMP/failing_tests_report "$TEMP/failing_tests"
+$PY_COMMAND $PYSCRIPT_PATH/get_failing_test_methods.py $TEMP/failing_tests_report "$TEMP/failing_tests"
 
 # step 3: get all classes
-python $PYSCRIPT_PATH/get_java_classes_from_directory.py "$TARGETPROJECT/$(defects4j export -w "$TARGETPROJECT" -p dir.src.classes)" "$TEMP/all_classes"
+$PY_COMMAND $PYSCRIPT_PATH/get_java_classes_from_directory.py "$TARGETPROJECT/$(defects4j export -w "$TARGETPROJECT" -p dir.src.classes)" "$TEMP/all_classes"
 
 # step 4-5: run and get coverage all failing tests
 CVR_OUTPUT_FOLDER="$TEMP/single_test_coverage_out"
@@ -31,15 +49,15 @@ number_of_out=$(find $CVR_OUTPUT_FOLDER/* -maxdepth 0 -type d | wc -l)
 while [ $counter -le `expr $number_of_out - 1` ]
 do
     cvr_folder="$CVR_OUTPUT_FOLDER/$counter/cobertura"
-    python $PYSCRIPT_PATH/get_methods_coverage.py $cvr_folder/coverage.xml "$MTD_OUTPUT_FOLDER/covered_methods-$counter"
+    $PY_COMMAND $PYSCRIPT_PATH/get_methods_coverage.py $cvr_folder/coverage.xml "$MTD_OUTPUT_FOLDER/covered_methods-$counter"
     ((counter++))
 done
-python $PYSCRIPT_PATH/aggregate_results.py $MTD_OUTPUT_FOLDER/ "$TEMP/all_covered_methods"
+$PY_COMMAND $PYSCRIPT_PATH/aggregate_results.py $MTD_OUTPUT_FOLDER/ "$TEMP/all_covered_methods"
 
 # step 7 get all covered classes
-python $PYSCRIPT_PATH/get_classes_coverage.py "$TEMP/all_covered_methods" "$TEMP/all_covered_classes"
+$PY_COMMAND $PYSCRIPT_PATH/get_classes_coverage.py "$TEMP/all_covered_methods" "$TEMP/all_covered_classes"
 # step 7.5 get all tests and then the passing tests
-python $PYSCRIPT_PATH/get_passing_test_methods.py "$TEMP/all_tests" "$TEMP/failing_tests" "$TEMP/passing_tests"
+$PY_COMMAND $PYSCRIPT_PATH/get_passing_test_methods.py "$TEMP/all_tests" "$TEMP/failing_tests" "$TEMP/passing_tests"
 
 # step 8-9 run coverage of passing tests with covered class instrumented
 PASS_OUTPUT_FOLDER="$TEMP/passing_test_coverage_out"
@@ -51,9 +69,9 @@ number_of_out=$(find $PASS_OUTPUT_FOLDER/* -maxdepth 0 -type d | wc -l)
 while [ $counter -le `expr $number_of_out - 1` ]
 do
     cvr_folder="$PASS_OUTPUT_FOLDER/$counter/cobertura"
-    python $PYSCRIPT_PATH/get_methods_coverage.py $cvr_folder/coverage.xml "$cvr_folder/covered_methods"
+    $PY_COMMAND $PYSCRIPT_PATH/get_methods_coverage.py $cvr_folder/coverage.xml "$cvr_folder/covered_methods"
     ((counter++))
 done
 
 # step 10-11 generate matrix
-python $PYSCRIPT_PATH/generate_method_and_test_matrix.py "$PASS_OUTPUT_FOLDER/" "$TEMP/all_covered_methods" "$TEMP/passing_tests" "$TEMP/res.csv"
+$PY_COMMAND $PYSCRIPT_PATH/generate_method_and_test_matrix.py "$PASS_OUTPUT_FOLDER/" "$TEMP/all_covered_methods" "$TEMP/passing_tests" "$TEMP/res.csv"
