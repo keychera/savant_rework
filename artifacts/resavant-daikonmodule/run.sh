bfd7e2ef72fc06b5a90fbd2e60887147239a5601
keychera <kyogatama@gmail.com>
2019-12-09 07:36:38 +0900

    create script to run Daikon

--- artifacts/resavant-daikonmodule/run.sh
+++ artifacts/resavant-daikonmodule/run.sh
@@ -1,15 +1,31 @@
 
 source runconfig
 
+# check daikon
+if [ -z "$DAIKONDIR" ]
+then
+    echo 'Error: Daikon is either not installed' >&2
+    echo 'or the $DAIKONDIR to is not set!' >&2
+    exit 1
+else
+    DAIKON_JAR="$DAIKONDIR/daikon.jar"
+fi
+
+# compile target project
+CUR_DIR=$(pwd)
+cd $TARGETPROJECT
+defects4j compile
+cd $CUR_DIR
+
 CLASSES_TO_CMPL=$(find ./src/ -name "*.java")
 mkdir -p target
-$JAVAC7_PATH -cp "./lib/*" -d "./target/" $CLASSES_TO_CMPL
+$JAVAC7_PATH -cp "$LIB_FOLDER" -d "./target/" $CLASSES_TO_CMPL
 
 CLS_DIR=$(defects4j export -p dir.bin.classes -w $TARGETPROJECT)
 TEST_DIR=$(defects4j export -p dir.bin.tests -w $TARGETPROJECT)
 
-CLASS_PATHS="$TARGETPROJECT/$CLS_DIR:$TARGETPROJECT/$TEST_DIR:./lib/*:./target"
-SINGLETESTRUN="$JAVA7_PATH -cp "$CLASS_PATHS" SingleTestRunner"
+CLASS_PATHS="$TARGETPROJECT/$CLS_DIR:$TARGETPROJECT/$TEST_DIR:$LIB_FOLDER:./target"
+SINGLETESTRUN="$JAVA7_PATH -cp $CLASS_PATHS SingleTestRunner"
 
 #should fail
 $SINGLETESTRUN org.apache.commons.lang3.math.NumberUtilsTest::TestLang747
@@ -19,3 +35,10 @@ $SINGLETESTRUN org.apache.commons.lang3.AnnotationUtilsTest::testBothArgsNull
 
 #should pass
 $SINGLETESTRUN org.apache.commons.lang3.LocaleUtilsTest::testParseAllLocales
+
+TEST_RUNNER="SingleTestRunner"
+TEST_TO_RUN="org.apache.commons.lang3.AnnotationUtilsTest::testBothArgsNull"
+
+java -cp $CLASS_PATHS:$DAIKON_JAR daikon.DynComp $TEST_RUNNER $TEST_TO_RUN
+
+java -cp $CLASS_PATHS:$DAIKON_JAR daikon.Chicory --daikon --comparability-file=$TEST_RUNNER.decls-DynComp $TEST_RUNNER $TEST_TO_RUN
