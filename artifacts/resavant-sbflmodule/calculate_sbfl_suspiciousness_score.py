abbc92108b8102a3e209cd247049ea8d9425121a
keychera <kyogatama@gmail.com>
2019-12-21 17:04:14 +0900

    make script to calc all SBFL and print to file

--- artifacts/resavant-sbflmodule/calculate_sbfl_suspiciousness_score.py
+++ artifacts/resavant-sbflmodule/calculate_sbfl_suspiciousness_score.py
@@ -7,14 +7,29 @@ if len(sys.argv) >= 2:
 
     raw_stat_df = pd.read_csv(raw_stat_file, header=None)
 
-    row = 5
-    np = int(raw_stat_df[1][row])
-    np_not = int(raw_stat_df[2][row])
-    nf = int(raw_stat_df[3][row])
-    nf_not = int(raw_stat_df[4][row])
-
     strats = sbfl.all_sbfl_strategies
-    for strat in strats:
-        print(strat)
-        res = strat.calc(np, np_not, nf, nf_not)
-        print(res)
+    n_strat = len(strats)
+    n_method = len(raw_stat_df[0])
+    suspicousness_scores = pd.DataFrame(columns=range(0, n_strat + 1), index=range(0, n_method))
+    suspicousness_scores.iloc[:,0] = raw_stat_df[0]
+
+    for row in range(0, n_method):
+        np = int(raw_stat_df[1][row])
+        np_not = int(raw_stat_df[2][row])
+        nf = int(raw_stat_df[3][row])
+        nf_not = int(raw_stat_df[4][row])
+
+        i = 1
+        for strat in strats:
+            res = strat.calc(np, np_not, nf, nf_not)
+            suspicousness_scores.iloc[row:,i] = res
+            i += 1
+
+    if len(sys.argv) >= 3:
+        output_file = sys.argv[2]
+        suspicousness_scores.to_csv(path_or_buf=output_file, header=False, index=False)
+    else:
+        print(suspicousness_scores)
+
+else:
+    print('python calculate_sbfl_suspiciousness_score [raw stat file] (opt)[output path]')
--- artifacts/resavant-sbflmodule/run.sh
+++ artifacts/resavant-sbflmodule/run.sh
@@ -7,4 +7,4 @@ mkdir -p $TEMP/
 # $PY_COMMAND calculate_sbfl_raw_statistic.py "$TEMP_INPUT/res.csv" "$TEMP_INPUT/res_fail.csv" "$TEMP/raw_stat"
 
 # calculate given sbfl formula
-$PY_COMMAND calculate_sbfl_suspiciousness_score.py "$TEMP/raw_stat"
+$PY_COMMAND calculate_sbfl_suspiciousness_score.py "$TEMP/raw_stat" "$TEMP/susp_scores"
