c8d884b44ab542554601aa0b594ba0a81b650250
keychera <kyogatama@gmail.com>
2020-01-11 17:28:20 +0900

    add TestRunner (both Single and Multi)

---
+++ resavant/modules/daikon/TestRunner/src/TestRunner.java
@@ -0,0 +1,64 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Request;
+import org.junit.runner.Result;
+
+public class TestRunner {
+    public static void main(String... args) {
+        if (args.length == 2) {
+            String mode = args[0];
+            if (mode.equals("single")) {
+                String testName = args[1];
+                runTest(testName);
+            } else if (mode.equals("multiple")) {
+                String testListPath = args[1];
+                ArrayList<String> testList = readFileEachLine(testListPath);
+
+                Iterator<String> testIterator = testList.iterator();
+                while(testIterator.hasNext()) {
+                    String testName = testIterator.next();
+                    runTest(testName);
+                }
+            } else {
+                System.out.println("Wrong mode! [single|multiple]");
+            }
+
+        } else {
+            System.out.println("Wrong input");
+        }
+    }
+
+    private static void runTest(String testName) {
+        String[] classAndMethod = testName.split("::");
+        Request request;
+        try {
+            request = Request.method(Class.forName(classAndMethod[0]), classAndMethod[1]);
+            Result result = new JUnitCore().run(request);
+            System.out.println(result.wasSuccessful() ? "pass" : "fail");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static ArrayList<String> readFileEachLine(String path) {
+        ArrayList<String> lines = new ArrayList<>();
+        BufferedReader reader;
+		try {
+			reader = new BufferedReader(new FileReader(path));
+			String line = reader.readLine();
+			while (line != null) {
+				lines.add(line);
+				line = reader.readLine();
+			}
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+        return lines;
+    }
+}
--- resavant/modules/daikon/run.sh
+++ resavant/modules/daikon/run.sh
@@ -9,7 +9,12 @@ else
 fi
 
 # temp
-source $(dirname "$0")/runconfig
+JAVA7_PATH="/usr/java/jdk1.7.0_80/jre/bin/java"
+JAVAC7_PATH="/usr/java/jdk1.7.0_80/bin/javac"
+TARGETPROJECT="/home/square/Documents/projects/savant_rework/resavant/temp/0-checkout/Lang/1/b"
+TESTS_TO_RUN="/home/square/Documents/projects/savant_rework/resavant/modules/daikon/testlist"
+OUTPUT_DIR="$(dirname "$0")/temp"
+mkdir -p $OUTPUT_DIR
 
 # compile testrunners
 CLASSES_TO_CMPL=$(find $(dirname "$0")/TestRunner/src/ -name "*.java")
@@ -25,31 +30,34 @@ git clean -fd
 defects4j compile
 cd $CUR_DIR
 
-# prepare classpath and means to call SingleTestRunner
+# prepare classpath and means to call TestRunner
 CLS_DIR=$(defects4j export -p dir.bin.classes -w $TARGETPROJECT)
 TEST_DIR=$(defects4j export -p dir.bin.tests -w $TARGETPROJECT)
 
 CLASS_PATHS="$TARGETPROJECT/$CLS_DIR:$TARGETPROJECT/$TEST_DIR:$LIB_FOLDER:$TEST_RUNNER_DIR"
-SINGLETESTRUN="$JAVA7_PATH -cp $CLASS_PATHS SingleTestRunner"
+TEST_RUNNER="$JAVA7_PATH -cp $CLASS_PATHS TestRunner"
 
-echo "command that will be called: $SINGLETESTRUN"
+echo "command that will be called: $TEST_RUNNER"
 echo ""
 
 # should fail
-$SINGLETESTRUN org.apache.commons.lang3.math.NumberUtilsTest::TestLang747
+$TEST_RUNNER single org.apache.commons.lang3.math.NumberUtilsTest::TestLang747
 
 # should pass
-$SINGLETESTRUN org.apache.commons.lang3.AnnotationUtilsTest::testBothArgsNull
+$TEST_RUNNER single org.apache.commons.lang3.AnnotationUtilsTest::testBothArgsNull
 
 # should pass
-$SINGLETESTRUN org.apache.commons.lang3.LocaleUtilsTest::testParseAllLocales
+$TEST_RUNNER single org.apache.commons.lang3.LocaleUtilsTest::testParseAllLocales
 
-TEST_RUNNER="SingleTestRunner"
-TEST_TO_RUN="org.apache.commons.lang3.LocaleUtilsTest::testParseAllLocales"
+# multiple test in one run
+$TEST_RUNNER multiple $TESTS_TO_RUN
 
-echo "testing $TEST_TO_RUN"
+TEST_RUNNER_CLASS="TestRunner"
+DTRACE_FILE="$OUTPUT_DIR/test.dtrace.gz"
+INV_FILE="$OUTPUT_DIR/test.inv.gz"
+PRINT_INV_FILE="$OUTPUT_DIR/test-inv"
+$JAVA7_PATH -cp $CLASS_PATHS:$DAIKON_JAR daikon.Chicory --ppt-select-pattern="org.apache.commons.lang3" --dtrace-file=$DTRACE_FILE $TEST_RUNNER_CLASS multiple $TESTS_TO_RUN >/dev/null 2>&1
 
-DTRACE_FILE="test.dtrace.gz"
-$JAVA7_PATH -cp $CLASS_PATHS:$DAIKON_JAR daikon.Chicory --ppt-select-pattern="org.apache.commons.lang3" --dtrace-file=$DTRACE_FILE $TEST_RUNNER $TEST_TO_RUN >/dev/null 2>&1
+$JAVA7_PATH -cp $CLASS_PATHS:$DAIKON_JAR daikon.Daikon $DTRACE_FILE -o $INV_FILE >/dev/null 2>&1
 
-$JAVA7_PATH -cp $CLASS_PATHS:$DAIKON_JAR daikon.Daikon $DTRACE_FILE
+$JAVA7_PATH -cp $CLASS_PATHS:$DAIKON_JAR daikon.PrintInvariants $INV_FILE > $PRINT_INV_FILE
