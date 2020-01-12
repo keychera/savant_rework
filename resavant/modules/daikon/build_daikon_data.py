84512aeee008e130a4ab2a7831eae90ded1f740a
keychera <kyogatama@gmail.com>
2020-01-12 15:15:13 +0900

    add scripts to build daikon data

---
+++ resavant/modules/daikon/build_daikon_data.py
@@ -0,0 +1,61 @@
+import argparse
+import pandas as pd
+from pathlib import Path
+
+# parse arguments
+parser = argparse.ArgumentParser(description='Get inputs')
+parser.add_argument('--clusters', dest='clusters_dir', type=str, required=True,help='clusters dir')
+parser.add_argument('--coverage_stat', dest='coverage_stat_dir', type=str, required=True, help='coverage stat dir')
+parser.add_argument('--out_dir', dest='out_dir', type=str, required=True, help='output dir')
+
+args = parser.parse_args()
+cvr_stat_dir = args.coverage_stat_dir
+clusters_dir = args.clusters_dir
+out_dir = args.out_dir
+
+# get list of covered method
+matrix_failing_path = '{}/{}'.format(cvr_stat_dir, 'matrix_failing.csv')
+matrix_failing_df = pd.read_csv(matrix_failing_path, header=None)
+covered_method = matrix_failing_df[0]
+
+# get list passing tests
+
+passing_test_path = '{}/{}'.format(cvr_stat_dir, 'matrix_passing.csv.header')
+passing_test = list()
+with open(passing_test_path) as fp:
+    for count, line in enumerate(fp):
+        passing_test.append(line.rstrip())
+
+# get clusters and selected passing tests
+clusters_path = '{}/{}'.format(clusters_dir, 'clusters')
+clusters = list()
+with open(clusters_path) as fp:
+    for count, line in enumerate(fp):
+        cluster = line.rstrip().split(',')
+        clusters.append(cluster)
+
+selected_tests_path = '{}/{}'.format(clusters_dir, 'selected_tests')
+selected_tests = list()
+with open(selected_tests_path) as fp:
+    for count, line in enumerate(fp):
+        tests = line.rstrip().split(',')
+        selected_tests.append(tests)
+
+assert (len(clusters) == len(selected_tests)), 'number of clusters must be the same as number of groups of selected tests'
+
+# build three sets of clusers
+for i, cluster, tests in zip(range(0, len(clusters)), clusters, selected_tests):
+    cluster_dir = '{}/{}'.format(out_dir, i)
+    Path(cluster_dir).mkdir(parents=True, exist_ok=True)
+
+    cluster_file = '{}/{}'.format(cluster_dir, 'clusters')
+    with open(cluster_file, 'w+') as fp:
+        for methodIndex in cluster:
+            methodName = covered_method[int(methodIndex)]
+            fp.write('{}\n'.format(methodName))
+    
+    test_file = '{}/{}'.format(cluster_dir, 'selected_tests')
+    with open(test_file, 'w+') as fp:
+        for testIndex in tests:
+            testName = passing_test[int(testIndex)]
+            fp.write('{}\n'.format(testName))
