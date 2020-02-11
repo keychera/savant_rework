26fa783850f3840e3c10cec5dc8ade08371a1b5c
keychera <kyogatama@gmail.com>
2020-02-11 15:41:31 +0900

    integrate daikon and decouple abs path strings

--- resavant/modules/daikon/run.sh
+++ resavant/modules/daikon/run.sh
@@ -1,18 +1,29 @@
-PROJECT="Lang"
-CLUSTERS="/home/square/Documents/projects/savant_rework/resavant/temp/3-cluster/$PROJECT/1"
-CVR_PATH="/home/square/Documents/projects/savant_rework/resavant/temp/2-coverage/$PROJECT/1"
+# process args
+print_usage() {
+  printf "Usage: ..."
+}
 
-OUT_DIR="$(dirname "$0")/temp$PROJECT"
-if [ -d "$OUT_DIR" ]; then
-    rm -r $OUT_DIR
+while getopts 'p:c:v:o:' flag; do
+  case "${flag}" in
+    p) TARGET_PROJECT="${OPTARG}" ;;
+    c) CLUSTERS="${OPTARG}" ;;
+    v) CVR_PATH="${OPTARG}" ;;
+    o) OUTPUT_DIR="${OPTARG}" ;;
+    *) print_usage
+       exit 1 ;;
+  esac
+done
+
+if [ -d "$OUTPUT_DIR" ]; then
+    rm -r $OUTPUT_DIR
 fi
-DAIKON_DATA_DIR="$OUT_DIR/1_daikon_data"
+
+DAIKON_DATA_DIR="$OUTPUT_DIR/1_daikon_data"
 mkdir -p $DAIKON_DATA_DIR
 
 python $(dirname "$0")/build_daikon_data.py --clusters $CLUSTERS --coverage_stat $CVR_PATH --out_dir $DAIKON_DATA_DIR
 
-TARGET_PROJECT="/home/square/Documents/projects/savant_rework/resavant/temp/0-checkout/$PROJECT/1/b"
-DAIKON_OUT_DIR="$OUT_DIR/2_daikon_result"
+DAIKON_OUT_DIR="$OUTPUT_DIR/2_daikon_result"
 mkdir -p $DAIKON_OUT_DIR
 
 # compile target project
@@ -24,7 +35,7 @@ cd $CUR_DIR
 
 $(dirname "$0")/run_daikon_iteration.sh -t $TARGET_PROJECT -d $DAIKON_DATA_DIR -o $DAIKON_OUT_DIR
 
-DAIKON_DIFF_DIR="$OUT_DIR/3_daikon_diff"
+DAIKON_DIFF_DIR="$OUTPUT_DIR/3_daikon_diff"
 mkdir -p $DAIKON_DIFF_DIR
 
 $(dirname "$0")/run_diff_iteration.sh -d $DAIKON_OUT_DIR  -o $DAIKON_DIFF_DIR
--- resavant/run_savant_train.sh
+++ resavant/run_savant_train.sh
@@ -7,11 +7,28 @@ source $SCRIPTS_PY/check_py.sh
 PROJECTS=(Chart Closure Math Time Lang)
 BUGS_NUM=(1 0 0 0 1)
 
-L2R_DATA_FOLDER="$OUT_FOLDER/5-l2r-data"
+L2R_DATA_FOLDER="$OUT_FOLDER/6-l2r-data"
 mkdir -p $L2R_DATA_FOLDER
 
+# Multric
+# i=0
+# while [ $i -lt ${#PROJECTS[@]} ] 
+# do
+#     # for every bug
+#     bug_id=1
+#     while [ $bug_id -le ${BUGS_NUM[$i]} ]
+#     do
+#         # get coverage
+#         MULTRIC_CVR_FOLDER="$OUT_FOLDER/2-coverage/${proj_id}/${bug_id}"
+#         mkdir -p $MULTRIC_CVR_FOLDER
+# 
+#         $DEFECTS4J_MODULE/run_coverage.sh -w "${CHECKOUT_FOLDER}/b" -o "$MULTRIC_CVR_FOLDER"
+#     done
+# done
+
+# Savant
 i=0
-while [ $i -lt ${#PROJECTS[@]} ] 
+while [ $i -lt ${#PROJECTS[@]} ]
 do
 
     # for every bug
@@ -48,12 +65,18 @@ do
         $PY_COMMAND $CLUSTER_MODULE/generate_method_clusters.py "$CVR_FOLDER/matrix_passing.csv" $MAX_CLUSTER_SIZE "$CLUSTER_FOLDER/clusters" 
         $PY_COMMAND $CLUSTER_MODULE/select_tests.py "$CVR_FOLDER/matrix_passing.csv" "$CLUSTER_FOLDER/clusters" $MAX_TEST_NUMBER "$CLUSTER_FOLDER/selected_tests"
 
+        # daikon
+        DAIKON_FOLDER="$OUT_FOLDER/4-daikon/${proj_id}/${bug_id}"
+        mkdir -p $DAIKON_FOLDER
+        TARGET_PROJECT="$CHECKOUT_FOLDER/b"
+
+        $DAIKON_MODULE/run.sh -p $TARGET_PROJECT -c $CLUSTER_FOLDER -v $CVR_FOLDER -o $DAIKON_FOLDER
+
         # sbfl
-        SBFL_FOLDER="$OUT_FOLDER/4-sbfl/${proj_id}/${bug_id}"
+        SBFL_FOLDER="$OUT_FOLDER/5-sbfl/${proj_id}/${bug_id}"
         mkdir -p $SBFL_FOLDER
         $SBFL_MODULE/run_sbfl_calculation.sh -p "$CVR_FOLDER/matrix_passing.csv" -f "$CVR_FOLDER/matrix_failing.csv" -o $SBFL_FOLDER
 
-        # daikon
 
         # build the l2r data
         $PY_COMMAND $L2R_MODULE/build_l2r_data.py "$GROUND_TRUTH_FOLDER/${proj_id}.${bug_id}b.method_diff" "$SBFL_FOLDER/susp_scores" "." "$L2R_DATA_FOLDER/l2rdata.${proj_id}.${bug_id}"
@@ -63,9 +86,9 @@ do
     
     ((i++))
 done
-    
+
 # l2r, aggregate the results and train
-L2R_RESULT_FOLDER="$OUT_FOLDER/6-l2r-result"
+L2R_RESULT_FOLDER="$OUT_FOLDER/7-l2r-result"
 mkdir -p $L2R_RESULT_FOLDER
 
 $L2R_MODULE/run_l2r_train.sh -i "$L2R_DATA_FOLDER" -o "$L2R_RESULT_FOLDER"
