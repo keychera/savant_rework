8ac71c74cf7af651ab351e25a226c37749b58258
keychera <kyogatama@gmail.com>
2020-03-03 22:15:22 +0900

    make timelog count to miliseconds

--- resavant/timelog.sh
+++ resavant/timelog.sh
@@ -1,22 +1,25 @@
-SECONDS=0
-LAST_TIME=0
+LAST_TIME=$(($(date +%s%N)/1000000))
 TIMELOG_FILE="$1"
 
 echo '' > $TIMELOG_FILE
+
 savant_timelog() {
     MSG=$1
-    CURRENT_TIME=$SECONDS
+    CURRENT_TIME=$(($(date +%s%N)/1000000))
     let "time_elapsed=(CURRENT_TIME-LAST_TIME)"
+    echo "elpase $time_elapsed"
     LAST_TIME=$CURRENT_TIME
 
     echo "$CURRENT_TIME $(date +"%T")" >> $TIMELOG_FILE
 
-    if (( $time_elapsed > 60 )) ; then
-        let "minutes=(time_elapsed%3600)/60"
-        let "seconds=(time_elapsed%3600)%60"
-        echo "$minutes minute(s) and $seconds second(s) for ($MSG)" >> $TIMELOG_FILE
+    let "seconds_elapsed=time_elapsed/1000"
+    let "mili_elapsed=time_elapsed%1000"
+    if (( $seconds_elapsed > 60000 )) ; then
+        let "minutes=(seconds_elapsed%3600)/60"
+        let "seconds=(seconds_elapsed%3600)%60"
+        echo "$minutes minute(s), $seconds second(s), $mili_elapsed miliseconds for ($MSG)" >> $TIMELOG_FILE
     else
-        echo "$time_elapsed seconds for ($MSG)" >> $TIMELOG_FILE
+        echo "$seconds_elapsed seconds, $mili_elapsed miliseconds for ($MSG)" >> $TIMELOG_FILE
     fi
 
 }
